816b5f1efef0b797647d8556667174be37f7577c
japanid133 <baguspw2405@gmail.com>
2020-11-14 19:26:15 +0700

    exception handling functionality

---
+++ resources/views/exception.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Kesalahan</title>
+    <link rel="stylesheet" href="{{asset(env('CSS_PATH').'style.min.css')}}">
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background-color: #FBFCFC;
+            height: 100%;
+            width: 100%;
+        }
+        #container {
+            margin: 0 auto;
+            width: 30vw;
+            display: flex;
+            flex-direction: column;
+            height: 100vh;
+        }
+        h1 {
+            font-size: 40pt;
+        }
+        .void-area {
+            flex: 1;
+        }
+        .fill-area {
+            flex: 0;
+        }
+    </style>
+</head>
+<body>
+<div id="container" class="">
+    <div class="void-area"></div>
+    <div class="fill-area">
+        <h1 class="text-danger"><strong>Kesalahan!</strong></h1>
+        <h2 class="text-danger opacity-7"><strong>@yield('error-type')</strong></h2>
+        <p class="text-justify">
+            @yield('error-message')
+        </p>
+        @yield('error-callback')
+    </div>
+    <div class="void-area"></div>
+</div>
+</body>
+</html>
