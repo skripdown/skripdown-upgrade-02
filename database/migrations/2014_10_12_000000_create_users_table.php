adb34eb13e28c8ea05f86fcb68de1bc32c8e0937
japanid133 <baguspw2405@gmail.com>
2020-11-16 17:27:32 +0700

    profile picture url for user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo_url')->default('asset/user/blank.jpg');
             $table->rememberToken();
             $table->timestamps();
         });
